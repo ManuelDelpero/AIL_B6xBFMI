6e280dc42e97096d323f10386aab230d972eed79
ManuelDelpero <you@example.com>
2019-04-29 12:02:40 +0200

    test

--- Quality Control/phenotypes.QC.R
+++ Quality Control/phenotypes.QC.R
@@ -6,31 +6,31 @@ measurementdays.num <- as.numeric(gsub("d", "", measurementdays.char))
 
 weight <- phenotypes[, measurementdays.char]
 
-#QC by weight
-plot(x = c(min(measurementdays.num), max(measurementdays.num)), 
-     y = c(0, max(weight * 1.15, na.rm=TRUE)), 
+# QC by weight
+plot(x = c(min(measurementdays.num), max(measurementdays.num)),
+     y = c(0, max(weight * 1.15, na.rm=TRUE)),
      main = "Individual growth curves",
      t = "n", yaxs = "i", xlab = "time(days)", ylab = "weight(gr.)")
 for (x in 1:nrow(phenotypes)) {
   points(measurementdays.num, weight[x,], t ="l", col = "orange")
 }
 
-#QC by weight
-plot(x = c(100, 140), 
-     y = c(0, max(weight * 1.15, na.rm=TRUE)), 
+# QC by weight
+plot(x = c(100, 140),
+     y = c(0, max(weight * 1.15, na.rm=TRUE)),
      main = "Individual growth curves",
      t = "n", yaxs = "i", xlab = "time(days)", ylab = "weight(gr.)")
 for (x in 1:nrow(phenotypes)) {
   points(measurementdays.num, weight[x,], t ="l", col = "orange")
 }
 
-# oralGTT
+# OralGTT
 oralGTT.cols <- paste0("GTT_", c(0, 15, 30, 60, 120))
 oralGTT <- phenotypes[, oralGTT.cols]
 oralGTT.num <- as.numeric(gsub("GTT_", "", colnames(oralGTT)))
 
-plot(main ="Oral Glucose Tolerance Test (GTT)", 
-     x = c(min(oralGTT.num), 
+plot(main ="Oral Glucose Tolerance Test (GTT)",
+     x = c(min(oralGTT.num),
      y = max(oralGTT.num)), c(0, max(oralGTT) * 1.15),  t = 'n', xlab="Time (min)", ylab="Blood Glucose (mg/dl)")
 
 for (x in 1:nrow(oralGTT)) {
@@ -40,13 +40,13 @@ for (x in 1:nrow(oralGTT)) {
 oralGTT.boxplot <- boxplot(oralGTT, main="Oral Glucose Tolerance Test", ylab="Blood Glucose (mg/dl)", xlab="Time(min)", col = "orange", notch=TRUE)
 lines(1:5, oralGTT.boxplot$stats[ 3, ], col="blue", lwd=2)
 
-# insulinTT
+# InsulinTT
 ITT.cols <-  paste0("ITT_", c(0,15,30,60))
 ITT <- phenotypes[, ITT.cols]
 ITT.num <- as.numeric(gsub("ITT_", "", colnames(ITT)))
 
-plot(main ="Insulin Tolerance Test (ITT)", 
-     x = c(min(ITT.num), max(ITT.num)), 
+plot(main ="Insulin Tolerance Test (ITT)",
+     x = c(min(ITT.num), max(ITT.num)),
      y = c(0, max(ITT) * 1.15),  t = 'n', xlab="Time (min)", ylab="Blood Glucose (mg/dl)")
 
 for (x in 1:nrow(ITT)){
@@ -56,7 +56,7 @@ for (x in 1:nrow(ITT)){
 ITT.boxplot <- boxplot(ITT, main="Insulin Tolerance Test", ylab="Blood Glucose (mg/dl)", xlab="Time(min)", col = "orange", notch=TRUE)
 lines(1:4, ITT.boxplot$stats[ 3, ], col="blue", lwd=2)
 
-#Tissues weight
+# Tissues weight
 tissues <- c("Leber", "WATgon", "WATsc")
 tissues.weights <- phenotypes[, tissues]
 ordering <- sort(tissues.weights[,"WATgon"], index.return=TRUE)$ix
@@ -75,4 +75,43 @@ legend("topleft",
   pt.cex = 2,
   cex = 1.2,
   text.col = "black")
-  
+
+# Calculate the area under the curve (auc) for each animal (oralGTT and insulinTT)
+library(DescTools)
+oralGTT <- phenotypes[, c("GTT_0",	"GTT_15",	"GTT_30",	"GTT_60",	"GTT_120")]
+insulinTT <- phenotypes[, c("ITT_0",	"ITT_15",	"ITT_30",	"ITT_60")]
+
+# InuslinTT
+aucs <- c()
+for(x in 1:nrow(insulinTT)){
+  aucs <- c(aucs, AUC(x = c(0, 15, 30, 60), as.numeric(insulinTT[x, 1:4])))
+}
+
+# Calculate adjusted auc
+# auc - base line auc (area from data at point 0 )
+
+ITTaucs.adj <- cbind(apply(insulinTT[, 1:4],2, as.numeric) - mean(as.numeric(insulinTT[, 1])))
+
+aucs.adjITT <- c()
+for(x in 1:nrow(ITTaucs.adj)){
+  aucs.adjITT <- c(aucs.adjITT, AUC(x = c(0, 15, 30, 60), as.numeric(ITTaucs.adj[x, 1:4])))
+}
+
+# OralGTT
+aucs <- c()
+for(x in 1:nrow(oralGTT)){
+  aucs <- c(aucs, AUC(x = c(0, 15, 30, 60, 120), as.numeric(oralGTT[x, 1:5])))
+}
+
+#calculate adjusted auc
+# auc - base line auc (area from data at point 0 )
+oralGTTaucs.adj <- cbind(apply(oralGTT[, 1:5],2, as.numeric) - mean(as.numeric(oralGTT[, 1])))
+
+aucs.adjGTT <- c()
+for(x in 1:nrow(oralGTTaucs.adj)){
+  aucs.adjGTT <- c(aucs.adjGTT, AUC(x = c(0, 15, 30, 60, 120), as.numeric(oralGTTaucs.adj[x, 1:5])))
+}
+
+#adding new values to the pheno matrix
+allPhenotypes <- cbind(phenotypes, aucs.adjGTT, aucs.adjITT)
+write.table(allPhenotypes, "allPhenotypes.txt", sep = "\t", quote=FALSE)
