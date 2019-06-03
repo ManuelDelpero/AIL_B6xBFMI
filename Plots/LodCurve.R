691e9596972fc827afeed0c53b5cc506c8ca8f15
ManuelDelpero <manueldlpro94@gmail.com>
2019-06-03 11:06:20 +0200

    I modified the file LodCurve.R

--- Plots/LodCurve.R
+++ Plots/LodCurve.R
@@ -1,4 +1,4 @@
-#lodscore curves 
+#lodscore curves
 
 setwd("C:/Users/Manuel/Desktop/AIL_B6xBFMI/RAWDATA")
 
@@ -27,30 +27,29 @@ lodannotmatrix <- cbind(annotation[rownames(lodmatrix), ], lodmatrix)
 #Dataset, containing columns named: Chr, Pos, marPvalue
 dataset <- lodannotmatrix[, c("Chromosome", "Position", "d63", "d70", "d98", "d140")]
 chr3 <- dataset[which(dataset[,"Chromosome"] == 3),]
-	
+
 plot(main = "Lod score curve Chr 3", c(min(as.numeric(chr3[, "Position"])), max(as.numeric(chr3[, "Position"]))), c(0,9), ylab = "Lodscore", xlab = "Position")
 	points(x = as.numeric(chr3[,"Position"]), y = chr3[,"d70"] , type = "l", col="dodgerblue", lwd = 1)
 	points(x = as.numeric(chr3[,"Position"]), y = chr3[,"d140"] , type = "l", col="blue", lwd = 1)
 	points(x = as.numeric(chr3[,"Position"]), y = chr3[,"d63"] , type = "l", col="deepskyblue", lwd = 1)
 	points(x = as.numeric(chr3[,"Position"]), y = chr3[,"d98"] , type = "l", col="dodgerblue4", lwd = 1)
 	abline(h=4.5, col="red")
-    legend("topleft",
-     legend = c("d63", "d70", "d98", "d140"),
+  legend("topleft",
+  legend = c("d63", "d70", "d98", "d140"),
      col = c("deepskyblue", "dodgerblue", "dodgerblue4", "blue"),
      pch = c(20,20,20),
      bty = "n",
      pt.cex = 2,
      cex = 1.2,
      text.col = "black")
-	
-	
-	
+
+
+
 #Chr 8
 #Dataset, containing columns named: Chr, Pos, marPvalue
 dataset <- lodannotmatrix[, c("Chromosome", "Position", "Triglycerides/Proteins")]
 chr3 <- dataset[which(dataset[,"Chromosome"] == 8),]
-	
+
 plot(main = "Lod score curve Chr 8", c(min(as.numeric(chr3[, "Position"])), max(as.numeric(chr3[, "Position"]))), c(0,7), ylab = "Lodscore", xlab = "Position")
 	points(x = as.numeric(chr3[,"Position"]), y = chr3[,"Triglycerides/Proteins"] , type = "l", col="orange", lwd = 3)
 	abline(h=4.5, col="red")
-	
